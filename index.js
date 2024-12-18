f82006ca192f9487b2c724e4a9f22d91cec44c9d
jahnad pr <jahanadputhankottil@gmail.com>
2024-12-18 17:35:20 +0530

    the congig updated for fix cors issueses

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 
 // Initial Middlewares
 app.use(cors({
-    origin: 'http://localhost:8080',  // Your frontend URL
+    origin: 'https://green-grocer-space.vercel.app',  // Your frontend URL
     credentials: true,  // Allow credentials (cookies)
 }));
 
