9cee776f09bf4f1643f56589ded84c084ca32829
jahnad pr <jahanadputhankottil@gmail.com>
2024-12-18 22:58:16 +0530

    chnaged theere

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 
 // Initial Middlewares
 app.use(cors({
-    origin:'https://green-grocer-shop.vercel.app/', // Your frontend URL
+    origin:'https://green-grocer-shop.vercel.app', // Your frontend URL
     credentials: true, // Allow credentials (cookies)
 }));
 
