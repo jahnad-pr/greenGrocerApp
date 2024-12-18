57f40445db3ca4efd1213f4842486060564587b3
jahnad pr <jahanadputhankottil@gmail.com>
2024-12-18 16:33:40 +0530

    the congig integrated once more

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 
 // Initial Middlewares
 app.use(cors({
-    origin: 'https://green-grocer-shops.vercel.app/',  // Your frontend URL
+    origin: 'https://green-grocer-shops.vercel.app',  // Your frontend URL
     credentials: true,  // Allow credentials (cookies)
 }));
 
