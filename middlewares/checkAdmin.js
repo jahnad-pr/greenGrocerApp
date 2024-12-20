cd8cc363ee958a934140826d184870c1f25305a9
jahnad pr <jahanadputhankottil@gmail.com>
2024-12-20 11:53:20 +0530

    the admin proble

--- middlewares/checkAdmin.js
+++ middlewares/checkAdmin.js
@@ -7,18 +7,18 @@ module.exports.adminAuthMiddleware = (req, res, next) => {
   
   // Get token from Authorization header
   const token = req.cookies.authkeys
-  console.log('tocken:',token);
   
-
+  
   if (!token) {
-
+    
     return res.status(401).json({ message: "No token provided, authorization denied" });
     
   }
-
+  
   try {
     // Verify token
     const decoded = jwt.verify(token, SECRET_KEY);
+    console.log('tocken:',SECRET_KEY);
 
     // Attach user to request
     req.admin = decoded; // `decoded` contains the user payload from the token
