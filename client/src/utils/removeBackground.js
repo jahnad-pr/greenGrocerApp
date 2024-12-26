f5788a5bdc039be16a8dfad1568d796018bb0cb2
jahnad pr <jahanadputhankottil@gmail.com>
2024-12-26 22:59:06 +0530

    finel setup

--- client/src/utils/removeBackground.js
+++ client/src/utils/removeBackground.js
@@ -1,12 +1,37 @@
-export const removeBackground = async (imageBlob) => {
-  const formData = new FormData();
-  formData.append('image_file', imageBlob);
+// export const removeBackground = async (imageBlob) => {
+//   const formData = new FormData();
+//   formData.append('image_file', imageBlob);
   
+//   try {
+//     const response = await fetch('https://api.remove.bg/v1.0/removebg', {
+//       method: 'POST',
+//       headers: {
+//         'X-Api-Key': 'vkMEHxH4VY8qZTgqTG3EBhor',
+//       },
+//       body: formData,
+//     });
+
+//     if (!response.ok) {
+//       throw new Error('Failed to remove background');
+//     }
+
+//     const blob = await response.blob();
+//     return blob;
+//   } catch (error) {
+//     console.error('Error removing background:', error);
+//     throw error;
+//   }
+// };
+
+export const removeBackgroundPhotoRoom = async (imageBlob) => {
+  const formData = new FormData();
+  formData.append('file', imageBlob);
+
   try {
-    const response = await fetch('https://api.remove.bg/v1.0/removebg', {
+    const response = await fetch('https://sdk.photoroom.com/v1/segment', {
       method: 'POST',
       headers: {
-        'X-Api-Key': 'vkMEHxH4VY8qZTgqTG3EBhor',
+        'x-api-key': '6feb44999cc2759b4eab702f7a756414abaaf2cd',
       },
       body: formData,
     });
@@ -22,3 +47,4 @@ export const removeBackground = async (imageBlob) => {
     throw error;
   }
 };
+
